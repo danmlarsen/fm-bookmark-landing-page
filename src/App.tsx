2263a895f312eb9e9615a4400891b9ed4e14ce9a
danmlarsen <larsendanmarius@gmail.com>
2024-10-12 11:01:04 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+    return <div className="min-h-screen grid place-items-center text-blue-900 font-semibold text-5xl">Hello world</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+    content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                blue: {
+                    500: 'hsl(231, 69%, 60%)',
+                    900: 'hsl(229, 31%, 21%)',
+                },
+                red: 'hsl(0, 94%, 66%)',
+                grayishBlue: 'hsl(229, 8%, 60%)',
+            },
+        },
+    },
+    plugins: [],
+};
